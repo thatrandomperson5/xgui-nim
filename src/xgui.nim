d3c372918ffa5580149076f5a5331689830326f1
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2023-01-08 17:15:26 +0000

    Added more docs

--- src/xgui.nim
+++ src/xgui.nim
@@ -1,4 +1,27 @@
 import xgui/[nn, defaults], std/[tables]
+## =====
+## XGui
+## =====
+## The xml/gui library, load nigui's from xml
+##
+## Basics
+## ======
+## - The tag name is the type, eg `Button`, `LayoutContainer`
+## - The attributes are the fields on the object
+## - The text is an alias for the `text` field.
+##
+## Special attributes
+## ===================
+## Special attributes are marked in this format: `X{type}-{attr name}`
+## - `XI-` forces int.
+## - `XS-` forces string.
+## - `XD-` forces ident.
+## 
+## Special Tags
+## ==============
+## - `link` replaces the link tah with the contents of a file linked with `ref=`
+## - `script` write nim in xml, special `parent` let returns the parent of the script tag's obj
+##
 
 
 const xmlAliases* = {"l": "Label"}.toTable
