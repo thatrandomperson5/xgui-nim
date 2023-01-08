db47fc27f77a3bac239cd3614dd339b7d441b0ac
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2023-01-08 14:27:19 -0600

    Update xgui.nim

--- src/xgui.nim
+++ src/xgui.nim
@@ -13,40 +13,42 @@ import xgui/[nn, defaults], std/[tables]
 ##
 ## Examples
 ## =========
-## Basic nim code:
+## Basic nim code
+##
 ## .. code:: nim
 ##
-## import xgui, nigui
+##  import xgui, nigui
 ## 
-## app.init() # Init the nigui app
+##  app.init() # Init the nigui app
 ## 
-## let w = loadGui("basic.xml") # Load the xml into nigui objects
-## w.width = 600.scaleToDpi # Set the height
-## w.height = 400.scaleToDpi # Set the width
+##  let w = loadGui("basic.xml") # Load the xml into nigui objects
+##  w.width = 600.scaleToDpi # Set the height
+##  w.height = 400.scaleToDpi # Set the width
 ##
-## w.show() # Show the window
+##  w.show() # Show the window
 ## 
-## app.run() # Run the app
+##  app.run() # Run the app
 ##
 ## ..
 ##
 ## Basic xml
+## 
 ## .. code: xml
 ##
-## <Window title="NiGui &amp; XGui Example">
-##   <LayoutContainer>
-##     <Button tag="btn">
-##       Button 1
-##     </Button>
-##     <TextArea tag="txt"></TextArea>
-##     <script>
-##       getTag("btn").onClick = proc (event: ClickEvent) = # Get the button obj and add an event callback
-##         getTag("txt").addLine("Button 1 clicked, message box opened.") # Add a line to the TextArea
-##         getTag("window").alert("This is a simple message box.") # Open a alert on the top-level Window
-##         getTag("txt").addLine("Message box closed.") 
-##     </script>
-##   </LayoutContainer>
-## </Window>
+##   <Window title="NiGui &amp; XGui Example">
+##       <LayoutContainer>
+##          <Button tag="btn">
+##             Button 1
+##          </Button>
+##          <TextArea tag="txt"></TextArea>
+##          <script>
+##            getTag("btn").onClick = proc (event: ClickEvent) = # Get the button obj and add an event callback
+##              getTag("txt").addLine("Button 1 clicked, message box opened.") # Add a line to the TextArea
+##              getTag("window").alert("This is a simple message box.") # Open a alert on the top-level Window
+##              getTag("txt").addLine("Message box closed.") 
+##          </script>
+##       </LayoutContainer>
+##   </Window>
 ##
 ## ..
 ##
