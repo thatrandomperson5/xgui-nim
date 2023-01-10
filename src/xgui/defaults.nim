9e276f3aa171cbd29e5796d033e1cbc6ca281169
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2023-01-10 13:42:52 -0600

    potential fix

--- src/xgui/defaults.nim
+++ src/xgui/defaults.nim
@@ -5,14 +5,19 @@ template newLayoutContainer*(): LayoutContainer = newLayoutContainer(Layout_Vert
 proc newBold*(): Label = 
   let l = newLabel()
   l.setFontBold(true)
+  l.widthMode = WidthMode_Expand
   return l
 
 proc newHeader*(): Label = 
   let l = newLabel()
   l.setFontSize(app.defaultFontSize*2)
+  l.widthMode = WidthMode_Expand
+  l.width = l.width * 2
   return l
 
 proc newSubHeader*(): Label = 
   let l = newLabel()
   l.setFontSize(app.defaultFontSize*1.5)
+  l.widthMode = WidthMode_Expand
+  l.width = l.width * 1.5
   return l
